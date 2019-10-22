ecba4191013ada2b7bfb41cfd1348fb6c1dfeaa3
marcela garizao <marchsantrich@hotmail.com>
2019-10-21 19:45:15 -0500

    moved to vars

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   
-  def index; end
+  def index
+    if current_user
+      @timeline = current_user.timeline
+      @users = User.all
+    end
+  end
 end
